382c756ece38b843d0b0b5e656331a3057d55470
Loger <chenle199509@qq.com>
2019-06-21 00:06:21 +0800

    'happy;

---
+++ src/main/java/springsecurity/springsecurity/config/AdminUserEntity.java
@@ -0,0 +1,19 @@
+package springsecurity.springsecurity.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AdminUserEntity {
+
+    private Integer id;
+
+    private String username;
+
+    private String password;
+
+
+}
